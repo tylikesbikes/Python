60ed993625fe3c119989188caf51e7af47434bc4
Ty <tdewitt2@gmail.com>
2022-05-24 00:22:34 -0400

    adding python syntax exercise 1

---
+++ syntax_ex1/count_up.py
@@ -0,0 +1,20 @@
+def count_up(start, stop):
+    for num in range(start,stop+1):
+        print(num)
+    """Print all numbers from start up to and including stop.
+
+    For example:
+
+        count_up(5, 7)
+
+   should print:
+
+        5
+        6
+        7
+    """
+
+    # YOUR CODE HERE
+
+
+count_up(5, 7)        
